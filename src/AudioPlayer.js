a14074759b25b4380c0c9d34688fd86d87edd02d
xmoli <fromlili@outlook.com>
2021-03-02 21:45:34 +0800

    Confirm the best value

--- src/AudioPlayer.js
+++ src/AudioPlayer.js
@@ -1,46 +1,51 @@
 import RealTimeBPMAnalyzer from 'realtime-bpm-analyzer'
+import Plotly from 'plotly.js-dist'
+
 import { useState } from 'react'
+
+function initAnalyzer(audio, setBpm) {
+    // Create new instance of AudioContext
+    const audioContext = new AudioContext();
+    // Set the source with the HTML Audio Node
+    const source = audioContext.createMediaElementSource(audio);
+    // Set the scriptProcessorNode to get PCM data in real time
+    const scriptProcessorNode = audioContext.createScriptProcessor(4096, 1, 1);
+    // Connect everythings together
+    scriptProcessorNode.connect(audioContext.destination);
+    source.connect(scriptProcessorNode);
+    source.connect(audioContext.destination);
+
+    const onAudioProcess = new RealTimeBPMAnalyzer({
+        scriptNode: {
+            bufferSize: 4096,
+            numberOfInputChannels: 1,
+            numberOfOutputChannels: 1
+        },
+        pushTime: 2000,
+        pushCallback: (err, bpm) => {
+            if (bpm){
+                setBpm(bpm)
+            }
+        }
+    });
+    // Attach realTime function to audioprocess event.inputBuffer (AudioBuffer)
+    scriptProcessorNode.onaudioprocess = (e) => {
+        onAudioProcess.analyze(e);
+    };
+}
 
 function AudioPlayer(){
     const [Info, setInfo] = useState('')
-    const [bpm, setBpm] = useState([].fill({tempo: 0},0, 4))
-    
+    const [bpm, setBpm] = useState()
+
     function handleLoadAudioSource (e){
         let file = e.target.files[0]
-        setInfo(e.target.value)
+        setInfo(file.name)
         let audio = document.getElementById('track')
         let src = URL.createObjectURL(file)
         audio.setAttribute("src", src)
 
-        // Create new instance of AudioContext
-        const audioContext = new AudioContext();
-        // Set the source with the HTML Audio Node
-        const source = audioContext.createMediaElementSource(audio);
-        // Set the scriptProcessorNode to get PCM data in real time
-        const scriptProcessorNode = audioContext.createScriptProcessor(4096, 1, 1);
-        // Connect everythings together
-        scriptProcessorNode.connect(audioContext.destination);
-        source.connect(scriptProcessorNode);
-        source.connect(audioContext.destination);
-
-        const onAudioProcess = new RealTimeBPMAnalyzer({
-            scriptNode: {
-                bufferSize: 4096,
-                numberOfInputChannels: 1,
-                numberOfOutputChannels: 1
-            },
-            pushTime: 2000,
-            pushCallback: (err, bpm) => {
-                if (bpm){
-                    setBpm(bpm)
-                    console.log(bpm)
-                }
-            }
-        });
-        // Attach realTime function to audioprocess event.inputBuffer (AudioBuffer)
-        scriptProcessorNode.onaudioprocess = (e) => {
-            onAudioProcess.analyze(e);
-        };
+        initAnalyzer(audio, setBpm)
     }
 
     let AddFile = ()=>(<div>
@@ -52,7 +57,7 @@ function AudioPlayer(){
         style={{fontSize: '50px', fontWeight:'bold'}}
         >+</label>
     </div>)
-
+    
     return (
         <div>
             <AddFile/>
@@ -67,7 +72,10 @@ function AudioPlayer(){
 
             <div id="print">
                 <p>{Info}</p>
-                BPM: {bpm.map((v,i) => <span key={i.toString()}>{v.tempo} </span>)}
+                BPM: {bpm ? bpm.map((v,i) => <span key={i.toString()}>{v.tempo} </span>) : 0}
+                <p>
+                Best: {bpm ? bpm.reduce((a,b) =>(a.count>b.count? a:b)).tempo: 0}
+                </p>
             </div>
         </div>
     )
